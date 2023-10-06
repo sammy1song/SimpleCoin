fe0771eb3c6c7beca99ce127bcf950aca1235f52
Saekyul Sammy Song <sammy.song259@gmail.com>
2023-10-06 08:05:30 -0500

    Implemented basic Proof-of-Stake (PoS) consensus with staking and validator rewards.

--- simplecoin.py
+++ simplecoin.py
@@ -1,5 +1,6 @@
 import hashlib
 import time
+import random
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import serialization, hashes
 
@@ -20,12 +21,14 @@ class Wallet:
         return public_key.verify(signature, transaction.encode(), ec.ECDSA(hashes.SHA256()))
 
 class Transaction:
-    def __init__(self, sender, recipient, amount, signature):
+    def __init__(self, sender, recipient, amount, signature, tx_type="transfer", fee=1):
         self.sender = sender
         self.recipient = recipient
         self.amount = amount
         self.signature = signature
         self.fee = fee
+        self.tx_type = tx_type  # New attribute to define the type of transaction
+
 
     def __str__(self):
         return "{}{}{}{}".format(self.sender, self.recipient, self.amount, self.fee)
@@ -53,6 +56,33 @@ class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
         self.transaction_pool = []
+        self.stakers = {} # New dictionary to keep track of staked coins
+
+    def stake_coins(self, address, amount):
+        # Check if the user has enough balance
+        balance = self.get_balance(address)
+        if balance < amount:
+            print("Insufficient funds to stake!")
+            return False
+        # Add or update the staked amount
+        if address in self.stakers:
+            self.stakers[address] += amount
+        else:
+            self.stakers[address] = amount
+        # Create a staking transaction
+        staking_transaction = Transaction(address, "STAKE_ADDRESS", amount, None, tx_type="stake")
+        self.transaction_pool.append(staking_transaction)
+        return True
+
+    def select_validator(self):
+        # Select a validator based on the number of coins staked
+        total_staked = sum(self.stakers.values())
+        r = random.randint(1, total_staked)
+        for address, stake in self.stakers.items():
+            if r <= stake:
+                return address
+            r -= stake
+
 
     def add_transaction_to_pool(self, transaction, sender_public_key, signature):
         # Added validation for transaction amounts
@@ -75,11 +105,7 @@ class Blockchain:
     def create_genesis_block(self):
         return Block("0", [])
 
-    def add_transaction_to_pool(self, transaction, sender_public_key, signature):
-        if Wallet.verify_signature(sender_public_key, signature, str(transaction)):
-            self.transaction_pool.append(transaction)
-
-    def mine_block(self, miner_address):
+    def mine_block(self, miner_address=None):
         if not self.transaction_pool:
             return False
 
@@ -88,16 +114,23 @@ class Blockchain:
         transactions_to_add = sorted_transactions[:MAX_TRANSACTIONS_PER_BLOCK]
         new_block = Block(self.chain[-1].hash, transactions_to_add)
         new_block.mine()  # Proof-of-Work mining
-        self.chain.append(new_block)
+
+        # PoS: Select a validator based on staked coins
+        validator = self.select_validator()
+        # Reward the validator
+        reward_transaction = Transaction("Network", validator, MINING_REWARD, None, tx_type="reward")
+        self.transaction_pool.append(reward_transaction)
 
         # Clear the added transactions from the pool
         self.transaction_pool = self.transaction_pool[MAX_TRANSACTIONS_PER_BLOCK:]
 
         # Adjusted reward mechanism
         global MINING_REWARD
-        self.transaction_pool.append(Transaction("Network", miner_address, MINING_REWARD, None))
         MINING_REWARD *= 0.9  # Reward reduction mechanism
+
+        self.chain.append(new_block)
         return True
+
     
     def get_balance(self, address):
         # Calculate the balance of an address
